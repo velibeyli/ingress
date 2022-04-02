7050c11caee1dc354ff82aefae3509197ad3001a
velibeyli <valibayli91@gmail.com>
2022-04-02 17:33:03 +0400

    80% of main page of ingress site added github

--- Mold/assets/js/main.js
+++ Mold/assets/js/main.js
@@ -5,4 +5,22 @@
 $(document).ready(function () {
   ///////////////////////////////////////////////////////////////////////////////////////////
   // 01. Test
+
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
 });
