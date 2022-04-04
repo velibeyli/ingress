b04dcd40ce2d197e9426c014973fd59edb087a44
velibeyli <valibayli91@gmail.com>
2022-04-04 17:39:45 +0400

    main page of ingress website fully completed

--- ingress/assets/js/main.js
+++ ingress/assets/js/main.js
@@ -39,10 +39,13 @@ $('.team_corusel').owlCarousel({
         0:{
             items:1
         },
-        600:{
+        768:{
             items:2
         },
         1000:{
+            items:3
+        },
+        1200:{
             items:4
         }
     }
